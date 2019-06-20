f6a0c5005fbac925c7a7a530026f246888b901e7
RuanJianXinCheng <woon93@163.com>
2019-06-20 17:29:13 +0800

    访问图片测试

--- src/main/java/com/wrong/demo1/DemoViewController.java
+++ src/main/java/com/wrong/demo1/DemoViewController.java
@@ -10,26 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/demoView")
 public class DemoViewController {
 
-//    @Autowired
-//    IDemoViewService demoViewService;
-
-    @RequestMapping("/thymeleaf")
-    public String testThymeleaf(Map<String,Object> map) {
-        map.put("msg", "Hello Thymeleaf");
-
-//        map.put("userList", demoViewService.testThymeleaf());
-
-        return "test_thymeleaf";
-    }
-
-    @RequestMapping("/freemaker")
-    public String testFreemaker(Map<String,Object> map) {
-        map.put("msg", "Hello freemarker!");
-        return "test_freemarker";
-    }
-
-    @RequestMapping("/index")
+    @RequestMapping("/pic")
     public String index() {
-        return "test_sidebar";
+        return "test_pic";
     }
 }
