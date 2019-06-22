8652090d47509658c5c991551ccc5e667d7e6111
woon9 <woon93@163.com>
2019-06-22 16:06:12 +0800

    tool update

--- src/main/java/tool/novelCrawler.java
+++ src/main/java/tool/novelCrawler.java
@@ -1,5 +1,6 @@
 package tool;
 
+import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -7,34 +8,65 @@ import org.jsoup.select.Elements;
 
 import java.io.*;
 import java.net.SocketTimeoutException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class novelCrawler {
     public static void main( String[] args ) throws IOException, InterruptedException {
-        Document doc = Jsoup.connect("https://www.dingdianxs.com/5/5188/").get();
-        Elements chapters = doc.select("div#list").select("a[href]");
+        // 本地存放路径
+        String localUrl = "E:\\小说\\";
+
+        // 小说名
+        String noverlName = "柳心眉慕容逸飞";
+
+        // 章节目录URL
+        String chapterUrlList = "https://www.biqugeso.com/book/13622/";
+
+        // 章节列表所在元素ID
+        String chaptersId = "list-chapterAll";
+
+        // 文本所在元素ID
+        String contextId = "htmlContent";
+
+        Document doc = Jsoup.connect(chapterUrlList).get();
+        // 读取章节列表所在元素内所有链接
+        Elements chapters = doc.select("div#" + chaptersId).select("a[href]");
         StringBuffer content = new StringBuffer();
         // 小说标题
-        content.append("摄政冷王悄医妃" + "\n");
-        for (int i = 900 ; i <= chapters.size()-1; i++) {
-//        for (Element chapter : chapters) {
+        content.append(noverlName + "\n");
+        // 节选章节（左开右闭）
+        List<Element> subElements= chapters.subList(3,6);
+
+        for (Element chapter : subElements) {
             // 章目标题
-            content.append( "\n\n\n" + chapters.get(i).text() + "\n");
-//            Thread.sleep(300);
+            content.append( "\n\n\n" + chapter.text() + "\n");
+            // 暂停进程（毫秒）
+            Thread.sleep(30);
             String txt = "";
+            // String txtTrimNbsp = "";
             try{
-                Document section = Jsoup.connect("https://www.dingdianxs.com/5/5188/" + chapters.get(i).attr("href")).get();
-                Elements element = section.select("div#content");
+                Document section = Jsoup.connect(chapterUrlList + chapter.attr("href")).get();
+                Elements element = section.select("div#" + contextId);
+
+                // 替换掉网页原文里的【&nbsp;】
+                // txtTrimNbsp = element.text().replace(Jsoup.parse("&nbsp;").text(), "");
+
                 txt = element.text();
-                txt.replace("     ","\r\n");
+                // 除去jsoup解析【&nbsp;】得到的ASCII值是160的空格
+                txt = txt.replace("\u00A0\u00A0\u00A0\u00A0","\n    ");
+
             } catch (SocketTimeoutException e){
-                System.err.println(chapters.get(i).text() + "读入失败！！！");
+                System.err.println(chapter.text() + "读入失败！！！   " + e.getMessage());
                 continue;
+            } catch (HttpStatusException e){
+                System.err.println(chapter.text() + "读入失败！！！--->" + e.getStatusCode());
+                break;
             }
             content.append(txt);
         }
 
         // 1：利用File类找到要操作的对象
-        File file = new File("E:\\小说\\摄政冷王悄医妃.txt");
+        File file = new File(localUrl +noverlName +".txt");
         if(!file.getParentFile().exists()){
             file.getParentFile().mkdirs();
         }
